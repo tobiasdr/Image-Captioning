8bdfc538f93cff6b287565636e6773a12e376e27
root <tobias.drees@gmail.com>
2020-01-17 10:23:03 +0000

    Decoder RNN architecture defined (init and forward)

--- model.py
+++ model.py
@@ -23,10 +23,22 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
+        super(DecoderRNN, self).__init__()
+        self.num_layers = num_layers
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        
+        self.embeddings = nn.Embedding(vocab_size, embed_size) 
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
+         
     def forward(self, features, captions):
-        pass
+        embeds = self.embeddings(captions[:,:-1])
+        inputs = torch.cat((features.unsqueeze(dim=1), embeds), dim=1)
+        lstm_output, states = self.lstm(inputs)
+        net_output = self.fc(lstm_output) 
+        return net_output
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
